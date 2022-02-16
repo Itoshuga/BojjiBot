796e26c474eb71073b9dade0aef3d0568ae6ee55
Itoshuga <38258159+Itoshuga@users.noreply.github.com>
2022-02-16 23:17:40 +0100

    Add files via upload

---
+++ Commands/Moderation/clear.js
@@ -0,0 +1,57 @@
+const { CommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "clear",
+    description: "Supprimer",
+    permission: "MANAGE_MESSAGES",
+    options: [
+        {
+            name: "amount",
+            description: "Nombre de message à supprimer.",
+            type: "NUMBER",
+            require: true
+        },
+        {
+            name: "target",
+            description: "Cible les messages d'une personne",
+            type: "USER",
+            require: false
+        },
+    ],
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const { channel, options } = interaction;
+
+        const Amount = options.getNumber("amount");
+        const Target = options.getMember("target");
+
+        const Messages = await channel.messages.fetch();
+
+        const Response = new MessageEmbed()
+            .setColor("BLUE");
+
+        if (Target) {
+            let i = 0;
+            const filtered = [];
+            (await Messages). filter((m) => {
+                if(m.author.id === Target.id && Amount > i) {
+                    filtered.push(m);
+                    i++;
+                }
+            })
+
+            await channel.bulkDelete(filtered, true).then(messages => {
+                Response.setDescription(`${messages.size} message(s) de ${Target} ont été supprimés.`);
+                interaction.reply({embeds: [Response]});
+            })
+        } else {
+            await channel.bulkDelete(Amount, true).then(messages => {
+                Response.setDescription(`${messages.size} message(s) du salon ont été supprimés.`);
+                interaction.reply({embeds: [Response]});
+            })
+        }
+    }
+}
